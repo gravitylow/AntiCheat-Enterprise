6b562df3e6849935f34559716a505d51dd260dbf
lDucks <lducksusa@gmail.com>
2013-11-11 21:34:50 -0500

    Remove files

--- website/js/main.js
+++ website/js/main.js
@@ -4,4 +4,12 @@ $(function(){
     $("#login").click(function(){
         $("#login-content").slideToggle();
     });
+    $("a[href='#colorblind'").click(function(){
+        var cur = $('#stylesheet').attr("href");
+        if(cur == "css/red.css") {
+            $('#stylesheet').attr("href", "css/colorblind.css");
+        } else {
+            $('#stylesheet').attr("href", "css/red.css");
+        }
+    });
 });
