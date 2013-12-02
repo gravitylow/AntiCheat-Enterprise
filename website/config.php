85fbfab3ed83312e171856fc1d3f8622ba0ef23f
lDucks <lducksusa@gmail.com>
2013-12-02 10:46:55 -0500

    Comments

--- website/config.php
+++ website/config.php
@@ -1,8 +1,10 @@
 <?php
 
-$url = 'localhost';
-$user = 'anticheatweb';
-$password = 'password';
-$database = 'anticheat';
+$url = 'localhost'; // URL to your MySQL server. If your MySQL server is on the same server as this webpage, leave this
+$user = 'anticheatweb'; // Database username
+$password = 'password'; // Database password
+$database = 'anticheat'; // Database name
+
+/* ------------ Do not edit below this line ------------ */
 
 $db = mysqli_connect($url, $user, $password, $database);
