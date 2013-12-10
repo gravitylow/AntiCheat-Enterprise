d5b86f9ff9750386c0f214ac4122e0b526dd8752
lDucks <lducksusa@gmail.com>
2013-12-10 17:47:33 -0500

    I'm gonna do this differently

--- website/install.php
+++ website/install.php
@@ -1,21 +1,16 @@
 <?php
 include('config.php');
-
 ?>
-    <div id="stepone">
-        <h2>Step One</h2>
-        Enter Superadmin Username:
-        <form>
-            <input type="text" /><br />
-            Enter Superadmin Password:
-            <input type="password" /><br />
-            Enter Superadmin Password (Again):
-            <input type="password" /><br />
-            <button>Continue</button>
-        </form>
-    </div>
-<?php
-
-
-
-?>
+<div id="stepone">
+    <h2>Step One</h2>
+    Enter Superadmin Username:
+    <form>
+        <input type="text" /><br />
+        Enter Superadmin Password:
+        <input type="password" /><br />
+        Enter Superadmin Password (Again):
+        <input type="password" /><br />
+        <button>Continue</button>
+    </form>
+</div>
+<div id="stream"></div>
