15b752a9b33a2f0664bde23eeda84ae8d949d5c5
lDucks <lducksusa@gmail.com>
2013-12-10 12:52:21 -0500

    Base for install.php

--- website/install.php
+++ website/install.php
@@ -1,4 +1,21 @@
 <?php
 include('config.php');
 
-// TODO: Create user table, then allow user to make superadmin account
+?>
+    <div id="stepone">
+        <h2>Step One</h2>
+        Enter Superadmin Username:
+        <form>
+            <input type="text" /><br />
+            Enter Superadmin Password:
+            <input type="password" /><br />
+            Enter Superadmin Password (Again):
+            <input type="password" /><br />
+            <button>Continue</button>
+        </form>
+    </div>
+<?php
+
+
+
+?>
