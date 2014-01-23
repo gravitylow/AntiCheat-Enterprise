eccd6c97fc88a2bb18fcf91647c379840ea85b4f
gravitylow <zero_gravity@outlook.com>
2014-01-22 20:07:00 -0500

    Use correct table name when clearing logs

--- website/util/ClearLogs.php
+++ website/util/ClearLogs.php
@@ -4,6 +4,6 @@ session_start();
 if($_SESSION['online']){
     require_once("../config.php");
 
-    $stmt = $db->prepare("TRUNCATE Logs");
+    $stmt = $db->prepare("TRUNCATE ac_logs");
     $stmt->execute();
 }
